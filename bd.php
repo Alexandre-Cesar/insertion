bbcc93649e87f70b871dd8d89ed478a42071aa6c
Alexandre-Cesar <alexandre.cesardf@hotmail.com>
2022-01-08 19:21:53 -0300

    primeira atualização do ano

---
+++ bd.php
@@ -0,0 +1,10 @@
+<?php
+     $servidor = 'localhost';
+     $user = "root";
+     $senha = "";
+     $db = "10";
+
+     $conexao = mysqli_connect($servidor, $user, $senha,$db);
+
+
+
---
+++ oper.php
@@ -0,0 +1,17 @@
+<?php
+ #criando uma classe
+class{
+   #parametros
+   public $objeto;
+   public $val;
+   public $descricao;
+
+   #metodos
+   public function texto(){
+      echo  "<br> tudo certo por aqui";
+   }
+   public function concat($val){
+      echo "juntei $valor com $val";
+   }
+}
+?>
